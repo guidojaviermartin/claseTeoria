8a257c069d8d2f99f388e0b21af9c98325d7cfe2
IvanM1995 <142050290+IvanM1995@users.noreply.github.com>
2024-03-21 18:41:18 -0300

    Update Participantes.java

--- claseTeoria/src/claseteoria/participantes.java
+++ claseTeoria/src/claseteoria/participantes.java
@@ -1,4 +1,13 @@
+<<<<<<< Updated upstream
 public class Participantes {
 //Guido Javier Martin Garcia
 //
+=======
+<<<<<<< HEAD
+public class Participantes {
+//Guido Javier Martin Garcia
+//Lisandro Adamek
+// Ivan Matto
+//Camila Biarnes 
+>>>>>>> Stashed changes
 }
